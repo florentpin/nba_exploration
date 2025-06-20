017c2d0c2d39d75330381cb3fdd5e8a8038c212b
Florent Pin <florentpin@gmail.com>
2025-06-20 15:20:48 +0200

    new version functions

--- app_nba.py
+++ app_nba.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 import plotly.graph_objects as go
-import functions
+import functions.py
 
 
 # Charger les données avec cache pour rapidité
